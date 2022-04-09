e5bbdab5b42931ac14319c97c42de9f5d8c6cd21
gjz22cn <gjz22cn@hotmail.com>
2022-04-09 19:48:27 +0800

    Develop | [formula] add formula activity

--- app/src/main/java/com/example/milkteamachine/MainActivity.kt
+++ app/src/main/java/com/example/milkteamachine/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.milkteamachine
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.AdapterView.OnItemClickListener
+import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,15 +17,22 @@ class MainActivity : AppCompatActivity() {
     private val milkTeaList: MutableList<MilkTea> = ArrayList<MilkTea>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
+        // 配方设置按钮
+        var formulaBtn =findViewById<Button>(R.id.btn_formula)
+        formulaBtn.setOnClickListener{
+            var intent = Intent(this@MainActivity, FormulaActivity::class.java)
+            startActivity(intent)
+        }
+
         // 先拿到数据并放在适配器上
-        initFruits() //初始化水果数据
+        initMilkTeas() //初始化数据
         val adapter = MilkTeaAdapter(this@MainActivity, R.layout.milktea_list_item, milkTeaList)
 
         // 将适配器上的数据传递给listView
-        val listView =
-            findViewById<ListView>(R.id.milktea_list_view)
+        val listView = findViewById<ListView>(R.id.milktea_list_view)
         listView.adapter = adapter
 
         // 为ListView注册一个监听器，当用户点击了ListView中的任何一个子项时，就会回调onItemClick()方法
@@ -31,18 +42,21 @@ class MainActivity : AppCompatActivity() {
                 val milkTea: MilkTea = milkTeaList[position]
                 Toast.makeText(this@MainActivity, milkTea.getName(), Toast.LENGTH_SHORT).show()
             }
+
+
     }
 
     // 初始化数据
-    private fun initFruits() {
-        val a = MilkTea("茉莉绿", "特大杯", "0")
-        milkTeaList.add(a)
-        val b = MilkTea("观音红", "大杯", "3分")
-        milkTeaList.add(b)
-        val c = MilkTea("果茶", "中杯", "5分")
-        milkTeaList.add(c)
-        val d = MilkTea("牛奶", "小杯", "7分")
-        milkTeaList.add(d)
-
+    private fun initMilkTeas() {
+        for (i in 0..8) {
+            val a = MilkTea("茉莉绿", "特大杯", "0")
+            milkTeaList.add(a)
+            val b = MilkTea("观音红", "大杯", "3分")
+            milkTeaList.add(b)
+            val c = MilkTea("果茶", "中杯", "5分")
+            milkTeaList.add(c)
+            val d = MilkTea("牛奶", "小杯", "7分")
+            milkTeaList.add(d)
+        }
     }
 }
